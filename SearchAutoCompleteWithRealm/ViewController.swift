5ae1479f7b3f13a27fef2d26fd5ff7d9ff1ca548
FameSprinter <kittitat_rpt@hotmail.com>
2017-10-09 17:08:05 +0700

    Initial Commit

---
+++ SearchAutoCompleteWithRealm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SearchAutoCompleteWithRealm
+//
+//  Created by Kittitat Rogphotong on 10/9/2560 BE.
+//  Copyright © 2560 FameSprinter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
