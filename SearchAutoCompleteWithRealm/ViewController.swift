3943eb6d90296f55cd955d95f0ed0c20a5f1c107
FameSprinter <kittitat_rpt@hotmail.com>
2017-10-11 13:35:25 +0700

    - Create `TableView` and `SearchController`.

--- SearchAutoCompleteWithRealm/ViewController.swift
+++ SearchAutoCompleteWithRealm/ViewController.swift
@@ -1,12 +1,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: - IBOutlet
+    @IBOutlet weak var tableView: UITableView!
+    
     // MARK: - Variable
     let viewModel = ViewModel(service: Service())
+    let searchController = UISearchController(searchResultsController: nil)
     
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        searchController.searchResultsUpdater = self
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.dimsBackgroundDuringPresentation = false
+        tableView.tableHeaderView = searchController.searchBar
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+extension ViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
         
     }
 }
