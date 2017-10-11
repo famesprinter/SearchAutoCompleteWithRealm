124fc23b859858c005ae0a98cc746c7203b1e108
FameSprinter <kittitat_rpt@hotmail.com>
2017-10-11 12:22:21 +0700

    - Add property in car model.

--- SearchAutoCompleteWithRealm/Car.swift
+++ SearchAutoCompleteWithRealm/Car.swift
@@ -2,5 +2,11 @@ import Foundation
 import RealmSwift
 
 class Car: Object {
+    @objc dynamic var id = ""
+    @objc dynamic var name = ""
     
+    func initCar(id: String, name: String) {
+        self.id = id
+        self.name = name
+    }
 }
