88d33278872262f13d1db841ea98379f84c762c9
FameSprinter <kittitat_rpt@hotmail.com>
2017-10-11 12:20:54 +0700

    - Create function `CreateNewCar` in Service.

--- SearchAutoCompleteWithRealm/Service.swift
+++ SearchAutoCompleteWithRealm/Service.swift
@@ -1,5 +1,12 @@
 import Foundation
+import RealmSwift
 
 struct Service: ServiceType {
+    let realm = try! Realm()
     
+    func createNewCar(car: Car) {
+        try! realm.write {
+            realm.add(car)
+        }
+    }
 }
